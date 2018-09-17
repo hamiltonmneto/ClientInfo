c93e625d930edba5fe7859e23155157bd0c9d0ad
Hamilton Monteiro <hamiltonmneto@gmail.com>
2018-09-17 01:37:11 -0300

    Added end point API

---
+++ InfoClient/InfoClient/Model/OSVersionInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoClient.Model
+{
+    public class OSVersionInfo
+    {
+        public string Name { get; set; }
+
+        public int Minor { get; set; }
+
+        public int Major { get; set; }
+
+        public int Build { get; set; }
+
+        public OSVersionInfo() { }
+    }
+}
